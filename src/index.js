58b9050ba82aa5b90076ace9a90db0d1e24aac36
djeam <60947080+djeam@users.noreply.github.com>
2021-01-11 08:22:34 -0500

    React Styling Practice Create a dynamic header which changes both color and greeting depending on the time of day.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const today = new Date();
+const currentTime = today.getHours();
+
+let greeting;
+
+const customStyle = {
+  color: ""
+};
+
+if (currentTime < 12) {
+  greeting = "Good morning";
+  customStyle.color = "red";
+} else if (currentTime < 18) {
+  greeting = "Good afternoon";
+  customStyle.color = "green";
+} else {
+  greeting = "Good evening";
+  customStyle.color = "blue";
+}
+
+ReactDOM.render(
+  <h1 className="heading" style={customStyle}>
+    {greeting}
+  </h1>,
+  document.getElementById("root")
+);
+
 //Create a React app from scratch.
 //Show a single h1 that says "Good morning" if between midnight and 12PM.
 //or "Good Afternoon" if between 12PM and 6PM.
